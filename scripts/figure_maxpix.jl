98032a000311ba75e35d7a5901d3f44309513870
DrWatson <no@mail>
2022-01-31 16:18:35 -0500

    Specify the KLM lines

--- scripts/figure_maxpix.jl
+++ scripts/figure_maxpix.jl
@@ -20,8 +20,27 @@ hs = NeXLSpectrum.compress(HyperSpectrum(
     fov = [ 4.096u"mm", 4.096u"mm"], offset= [ 0.0u"mm", 0.0u"mm" ]
 ))
 klms = [ 
-    n"C", n"Ag", n"Al", n"Ca", n"Ce", n"Cl", n"Cr", n"Cu", n"Fe", n"S", n"Ba", 
-    n"P", n"K", n"Mg", n"O", n"Mn", n"Na", n"Ni", n"Si", n"Ti", n"Zn"
+    n"C K-L2", #
+    characteristic(n"Ag", ltransitions, 0.1)..., #
+    n"Al K-L3", #
+    n"Ca K-L3", n"Ca K-M3", #
+    n"Ce L3-M5", # 
+    n"Cl K-L3", #
+    n"Cr K-L3", n"Cr K-M3", n"Cr L3-M5", #
+    n"Cu K-L3", n"Cu K-M3", n"Cu L3-M5", #
+    n"Fe K-L3", n"Fe K-M3", n"Fe L3-M5", #
+    n"S K-L3", n"S K-M3", #
+    characteristic(n"Ba", ltransitions, 0.03)..., #
+    n"P K-L3", #
+    n"K K-L3", #
+    n"Mg K-L3", #
+    n"O K-L3", #
+    n"Mn K-L3", n"Mn K-M3", n"Mn L3-M5", #
+    n"Na K-L3", #
+    n"Ni K-L3", n"Ni K-M3", n"Ni L3-M5", #
+    n"Si K-L3", #
+    n"Ti K-L3", n"Ti K-M3", n"Ti L3-M4", #
+    n"Zn K-L3", n"Zn K-M3", n"Zn L3-M5", #
 ]
 set_default_plot_size(8inch, 3inch)
 p1 = vstack(
